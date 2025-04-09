ba96d408dc43c7111358b93e6ce236dfa8c451f7
Chindi-M <179483549+Chindi-M@users.noreply.github.com>
2025-04-09 14:26:51 +0100

    chore: added .gitignore

--- tests/test_includes_todo.py
+++ tests/test_includes_todo.py
@@ -8,4 +8,12 @@ Task has to have #TODO
 
 Return all strings with #TODO"""
 
-def test
+# def test_check_if_string():
+#     # check if text is a string, force string input.
+#     result = includes_todo(text)
+#     return str
+
+def test_search_string():
+    # search a string, if #TODO present, return True
+    result = includes_todo('#TODO drinking coffee')
+    assert '#TODO drinking coffee' == True
